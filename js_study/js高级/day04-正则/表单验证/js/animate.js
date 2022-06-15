be39ee0dd647aeb4d6a2550095b842acae7017b9
ADun <2101580047@qq.com>
2022-06-15 09:26:20 +0000

    !1 阶段完结 * add LICENSE. * 数据结构 * 正则 * 换肤完善 * 完善文件夹

---
+++ js_study/js高级/day04-正则/表单验证/js/animate.js
@@ -0,0 +1,18 @@
+function animate(obj, target,callback) {
+  // 先清除以前的定时器，只保留当前的一个定时器执行
+  clearInterval(obj.timer);
+  obj.timer = setInterval(function (e) {
+    let steps = (target - obj.offsetLeft) / 10;
+    //这里没有取绝对值
+    steps = steps > 0 ? Math.ceil(steps) : Math.floor(steps);
+    if (obj.offsetLeft == target) {
+      clearInterval(obj.timer);
+      // if(callback){
+      //   callback();
+      // }
+      //简写
+      callback && callback();
+    }
+    obj.style.left = obj.offsetLeft + steps + 'px';
+  }, 15)
+}
